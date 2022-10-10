24d5fd0a44c370c6dceac988826759c90de0caa4
ppsett <ppsett@gmail.com>
2022-10-10 16:13:22 +0630

    complete OAuth funtionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,45 @@
 import {FcGoogle} from 'react-icons/fc'
 import React from 'react'
+import {toast} from 'react-toastify'
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {doc, setDoc, getDoc,serverTimestamp} from 'firebase/firestore'
+import { db } from '../firebase'
+import {useNavigate} from 'react-router-dom'
 
 export default function OAuth() {
+
+    const navigate = useNavigate();
+
+    async function onGoogleClick(){
+        try {
+            const auth = getAuth()
+            const provider = new GoogleAuthProvider()
+            const result = await signInWithPopup(auth, provider)
+            const user = result.user
+
+            //Check User exist
+
+            const docRef = doc(db, "users", user.uid)
+            const docSnap = await getDoc(docRef)
+            
+            if(!docSnap.exists()){
+                await setDoc(docRef, {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp()
+                });
+            }
+            navigate("/");
+
+        } catch (error) {
+            toast.error("Could not authorize with Google!")
+        }
+    }
+
   return (
-    <button className="flex items-center justify-center w-full
+    <button type="button"
+    onClick={onGoogleClick}
+    className="flex items-center justify-center w-full
     bg-red-700 text-white px-7 py-3 uppercase text-sm 
     font-medium hover:bg-red-800
     active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg
